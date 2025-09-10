e0ff42fc4d60715450ccfd93e5fe971f9a39f03f
Gyorgy David <gyorgy.david2006@gmail.com>
2025-09-10 11:10:11 +0200

    done, reset must do

--- script.js
+++ script.js
@@ -26,31 +26,49 @@ function ShowJokeByIdCard(){
         <label>Írja be a sorszámot: </label>
         <input type="number" id="jokeId" min="1" max="451">
         <button type="button" onclick="GetJokeById()">Generál</button>`
+}
 
-    
-    async function GetJokeById(){
-        let id = document.getElementById("jokeId").value;
+async function GetJokeById(){
+    let id = document.getElementById("jokeId").value;
 
-        try{
-            const response = await fetch(`https://official-joke-api.appspot.com/jokes/${id}`);
-            const data = await response.json();
-
-            jokeCard.innerHTML += `<p>${data.setup}</p>`;
-            jokeCard.innerHTML += `<p>${data.punchline}</p>`;
-        } catch (error){
-            console.error("Hiba történt:", error);
-        }
+    try{
+        const response = await fetch(`https://official-joke-api.appspot.com/jokes/${id}`);
+        const data = await response.json();
+        jokeCard.innerHTML += `<p>${data.setup}</p>`;
+        jokeCard.innerHTML += `<p>${data.punchline}</p>`;
+   
+    } catch (error){
+        console.error("Hiba történt:", error);
     }
 }
 
-function GetJokeByType(){
-    let type = document.getElementById("typeOfJoke").value;
-    console.log(type);
-    fetch('https://official-joke-api.appspot.com/jokes/random')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("jokeSetup").innerHTML = data.setup;
-        document.getElementById("jokePunchline").innerHTML = data.punchline;
-    })
-    .catch(error => console.error("Hiba történt:", error));
+function ShowJokeByType(){
+    let jokeCard = document.getElementById("jokeCard");
+
+    jokeCard.style.visibility = 'visible';
+    jokeCard.innerHTML = `<h2>Poén Típus Alapján</h2>`;
+    jokeCard.innerHTML += `
+        <label>Válassza ki a poén típusát: </label>
+        <select id="jokeType">
+            <option value="general">General</option>
+            <option value="programming">Programming</option>
+            <option value="knock-knock">Knock-Knock</option>
+            <option value="dad">Dad</option>
+        </select>
+        <button type="button" onclick="GetJokeByType()">Generál</button>`
+}
+
+async function GetJokeByType(){
+    let type = document.getElementById("jokeType").value;
+
+    try{
+        const response = await fetch(`https://official-joke-api.appspot.com/jokes/${type}/random`);
+        const data = await response.json();
+        
+        jokeCard.innerHTML += `<p>${data[0].setup}</p>`;
+        jokeCard.innerHTML += `<p>${data[0].punchline}</p>`;
+   
+    } catch (error){
+        console.error("Hiba történt:", error);
+    }
 }
